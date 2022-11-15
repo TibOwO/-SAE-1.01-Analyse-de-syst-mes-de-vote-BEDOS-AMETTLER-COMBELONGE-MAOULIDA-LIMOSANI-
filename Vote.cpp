ad1eb429e4f8f767ed00d0a637774766282e835a
TibOwO <thibaultamettler@gmail.com>
2022-11-16 00:12:23 +0100

    GOTO a faut changer la sémantique du goto a sinon on va se faire frpaper par casali il l'as dis dans son sujet

--- Vote.cpp
+++ Vote.cpp
@@ -7,23 +7,22 @@ using namespace std;
 int main()
 {
     cout << "Systeme ........" << endl;
-    string nomFichierEntree; // declaration de la variable du nom du fichier a ouvrir
-    bool fichierTrouve;      // déclara var pour boucler tant que le fichier n'est pas trouvé
-
-    while (!fichierTrouve) // tant que le fichier n'est pas trouvé
+    string nomFichierEntree;
+    bool fichierTrouve;
+a:
+    cout << "veuillez entrer le nom du fichier d'entree" << endl;
+    cin >> nomFichierEntree;
+    ifstream ifs;
+    ifs.open(nomFichierEntree);
+    if (ifs.is_open())
     {
-        cout << "veuillez entrer le nom du fichier d'entree" << endl;
-        cin >> nomFichierEntree;
-        ifstream ifs;               // declaration de ifs comme flux
-        ifs.open(nomFichierEntree); // ouvrir le fichier "nomFichierEntree" comme flux
-        if (ifs.is_open())          // on vérifie si il est ouvert
-        {
-            cout << "Fichier ouvert" << endl;
-            fichierTrouve = true; // permet de sortir de la boucle
-        }
-        else
-        {
-            cerr << "Fichier Introuvable" << endl; // afficher msg d'erreur et reviens au début de la boucle
-        }
+        cout << "Fichier ouvert" << endl;
+        fichierTrouve = true;
+        return 1;
+    }
+    else
+    {
+        cerr << "Fichier introuvable" << endl;
+        goto a;
     }
 }
