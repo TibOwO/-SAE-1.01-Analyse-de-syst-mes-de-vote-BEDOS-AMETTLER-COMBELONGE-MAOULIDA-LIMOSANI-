423c45b81608a33a1b0511e1859151d25c16d820
Enzo BEDOS <enzo.bedos@etu.univ-amu.fr>
2022-11-22 14:00:16 +0100

    not ifstream

--- Vote.cpp
+++ Vote.cpp
@@ -20,33 +20,33 @@ vector<int> votePluralite(int csgo, int mario, int civil, int streetfight)
 
 int main()
 {
-    cout << "Systeme ........" << endl;
+   // cout << "Systeme ........" << endl;
     string nomFichierEntree;
 a:
-    cout << "veuillez entrer le nom du fichier d'entree" << endl;
+    //cout << "veuillez entrer le nom du fichier d'entree" << endl;
     cin >> nomFichierEntree;
-    ifstream ifs;
-    ifs.open(nomFichierEntree);
-    if (ifs.is_open())
-    {
-        cout << "Fichier ouvert" << endl
-             << endl;
-    }
-    else
-    {
-        cerr << "Fichier introuvable" << endl;
-        goto a;
-    }
+    //ifstream ifs;
+    //cin.open(nomFichierEntree);
+    //if (cin.is_open())
+    //{
+    //    cout << "Fichier ouvert" << endl
+    //         << endl;
+    //}
+    //else
+    //{
+    //    cerr << "Fichier introuvable" << endl;
+    //    goto a;
+    //}
     int voteBlanc = 0;
     int CsGo = 0;
     int StreetFighter = 0;
     int CivilisationVI = 0;
     int MarioKart = 0;
 
-    while (ifs.eof() == false)
+    while (cin.eof() == false)
     {
         string str;
-        ifs >> str;
+        cin >> str;
 
         if (str == "csgo") // le test marche pas mais ca compte chaque ligne en gros
         {
