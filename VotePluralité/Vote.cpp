3b4aaf06f5cb22257ae6ccc9973c3172952d1703
Enzo BEDOS <enzo.bedos@etu.univ-amu.fr>
2022-11-25 14:36:39 +0100

    commantaire vote pluralité

--- VotePluralité/Vote.cpp
+++ VotePluralité/Vote.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 // déclaration des variables
-    int nbrParticiant = 0;               // Init. du compteur de participants/votants
+    int nbrParticipants = 0;             // Init. du compteur de participants/votants
 
     int jeu1, jeu2, jeu3, jeu4 = 0;      // Init. des compteurs de vote pour chaque jeu
 
@@ -34,77 +34,75 @@ void ListeJeu(string ligneLu, char delimiter)
 void affichJeux(){
     cout << "Les quatre jeu sont : " << endl;   // Affiche une première phrase
     string ligneJeu;                            // Init. variable ligneJeu de type string
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)                 // Boucle "for", lit la ligne, et enregistre la string lorsque que le curseur arrive au delimiter, 4 foix
     {
-        cin >> ligneJeu;
-        ListeJeu(ligneJeu, ' ');
-        nomJeux[i] = ligneJeu;
+        cin >> ligneJeu;                        // enregistre la ligne dns la variable "ligneJeu"
+        ListeJeu(ligneJeu, ' ');                // appel la fonction ListeJeu avec comme paramètre la ligneJeu et le delimiter
+        nomJeux[i] = ligneJeu;                  // une fois les 4 jeux séparé, ils sont enregistré dans un tableau "nomJeux"
     }
 }
 
 //-- Comptabilise les voix de chaque votant et les enregistres dans les variables coorespondantes
 void CompteVote(){
-    while (cin.eof() == false)
+    while (cin.eof() == false)              // boucle "while", tant que la boucle n'est pas arrive a la fin du fichier, la boucle continue.
     {
-        string ligneLue;
-        cin >> ligneLue;
-        if (ligneLue == "1")
+        string ligneLue;                    // Init. variable ligneLue de type string
+        cin >> ligneLue;                    // ligne suivante du fichier rentrée dans la variable ligneLue
+        if (ligneLue == "1")                // Condition "if", si ligneLue vaut "1", alors ...
         {
-            ++jeu1;
+            ++jeu1;                         // +1 a la variable jeu1
         }
 
-        else if (ligneLue == "2")
+        else if (ligneLue == "2")           // Condition "else if", sinon si ligneLue vaut "2", alors ...
         {
-            ++jeu2;
+            ++jeu2;                         // +1 à la variable jeu2
         }
 
-        else if (ligneLue == "3")
+        else if (ligneLue == "3")           // Condition "else if", sinon si ligneLue vaut "3", alors ...
         {
-            ++jeu3;
+            ++jeu3;                         // +1 à la variable jeu3
         }
 
-        else if (ligneLue == "4")
+        else if (ligneLue == "4")           // Condition "else if", sinon si ligneLue vaut "4", alors ...
         {
-            ++jeu4;
+            ++jeu4;                         // +1 à la variable jeu4
         }
 
-        else
+        else                                // Condition "else", sinon (si aucune des condition n'est verifiée) faire...
         {
-            ++nbrParticiant;
+            ++nbrParticipants;              // +1 a la variable nbrParticipants
         }
     }
-    nbrParticiant = nbrParticiant / 2;
+    nbrParticipants = nbrParticipants / 2;  // On divise le nombre de nbrParticipants car le compteur compte le nom ET prenom du votants.
 }
 
-// -- Affiche votes optenue pour chaque jeux --
+// -- Affiche les votes optenue pour chaque jeux --
 void affichResultat(){
-    // Affichage des résultats
-    cout << "Resultat final : " << endl
-         << endl
-         << nomJeux[0] << " = " << jeu1 << endl
-         << nomJeux[1] << " = " << jeu2 << endl
-         << nomJeux[2] << " = " << jeu3 << endl
-         << nomJeux[3] << " = " << jeu4 << endl
-         << "Nombre de participant = " << nbrParticiant << endl
-         << endl;
+    cout << "Resultat final : " << endl                             // Affiche "Resultat final : " avec un retour a la ligne
+         << endl                                                    // second retour a la ligne pour la mise en forme
+         << nomJeux[0] << " = " << jeu1 << endl                     // Affichage du premier jeu du tableau "nomJeu" avec son score et un retour a la ligne
+         << nomJeux[1] << " = " << jeu2 << endl                     // Affichage du second jeu du tableau "nomJeu" avec son score et un retour a la ligne
+         << nomJeux[2] << " = " << jeu3 << endl                     // Affichage du troisième jeu du tableau "nomJeu" avec son score et un retour a la ligne
+         << nomJeux[3] << " = " << jeu4 << endl                     // Affichage du quatrième jeu du tableau "nomJeu" avec son score et un retour a la ligne
+         << "Nombre de participant = " << nbrParticipants << endl   // Affichege du nombre de participants et un retour a la ligne
+         << endl;                                                   // second retour a la ligne pour la mise en forme
 }
 
 // -- Tri les jeux en fonction des vote reçu, du plus grand au plus petit --
 vector<int> CalculGagnant(int jeu1, int jeu2, int jeu3, int jeu4)
 {
-    vector<int> tab(4);
-    tab[0] = jeu1;
-    tab[1] = jeu2;
-    tab[2] = jeu3;
-    tab[3] = jeu4;
-    int i = 0;
-    sort(tab.begin(), tab.end(), greater<>());
-    return tab;
+    vector<int> tab(4);                         // Init. tableau d'entier comportant jusqu'à 4 valeur
+    tab[0] = jeu1;                              // la place 0 du tableau est jeu1
+    tab[1] = jeu2;                              // la place 1 du tableau est jeu2
+    tab[2] = jeu3;                              // la place 2 du tableau est jeu3
+    tab[3] = jeu4;                              // la place 3 du tableau est jeu4
+    sort(tab.begin(), tab.end(), greater<>());  // Tri l'entièreté du tableau du plus grand au plus petit
+    return tab;                                 // retourne le tableau trié
 }
 
 // -- Affiche le classement des jeux, du premier au dernier --
 void affichGagnant(vector<int> tab){
-    for (int i = 0; i <= tab.size() - 1; ++i)
+    for (int i = 0; i <= tab.size() - 1; ++i)               // Boucle for, 
     {
         if (i == 0)
         {
